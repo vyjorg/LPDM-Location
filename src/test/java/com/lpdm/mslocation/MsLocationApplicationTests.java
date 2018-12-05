11124a75246a115c8d96eea9097bff6fd376d198
vianney diris <vianney.diris@gmail.com>
2018-12-05 10:09:28 +0100

    Ajout du projet de base

---
+++ src/test/java/com/lpdm/mslocation/MsLocationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lpdm.mslocation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsLocationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
